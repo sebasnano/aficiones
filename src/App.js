807067da8310311b475827620310b1ca7a79a431
sebasnano <jsebrodriguezp@gmail.com>
2022-02-10 13:48:45 -0500

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const reyesGodos = [
+    {
+      nombre: "Ataúlfo",
+      aficion: "comer toros sin pelar",
+    },
+    {
+      nombre: "Recesvinto",
+      aficion: "leer a Hegel en arameo",
+    },
+    {
+      nombre: "Teodorico",
+      aficion: "la cría del escarabajo en almíbar",
+    },
+  ];
+
+  const [contador, setContador] = useState(0);
+  const [mensaje, setMensaje] = useState("");
+
+  const cambio = () => {
+    setContador(contador + 1);
+    if (contador + 1 >= reyesGodos.length) {
+      setContador(0);
+    }
+    setMensaje(
+      <h2>
+        La aficion principal del{" "}
+        <span className="rojo">{reyesGodos[contador].nombre}</span> es{" "}
+        <span className="verde">{reyesGodos[contador].aficion}</span>
+      </h2>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={cambio}>Ver Siguiente</button>
+      <div>{mensaje}</div>
+    </>
   );
-}
-
+};
 export default App;
